be85c10d4de0bd70c09f249d884dbfc1ef93136b
DANLCARTON <ericthiberge@orange.fr>
2023-10-01 16:38:58 +0200

    axioms aléatoires et gestion des crochets

--- index.js
+++ index.js
@@ -2,19 +2,61 @@ import * as THREE from "three"
 import { OrbitControls } from 'OrbitControls'; // importation de l'addon Orbit Controls pour la gestion de la caméra
 import { TrackballControls } from 'TrackballControls'; // importation de l'addon Orbit Controls pour la gestion de la caméra
 
-const axiom = "F*+*-*F/+F+F-FFF+FF/F*-*+F+F*+F+*F*-*+F-F*-*F+F*-*+F*+FF/+F/F-+*F++F/F-+FF-F/FF-F/FF/F+F/FF/F-F/FF/+F-F+*F*+F-F/FF/F-+FF*-*+F/+F/+*F-*F/F+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+"
-// const axiom = "F"
-// const axiom = "F-F+*F/F"
-
 const generateRandomPattern = (array) => {
     const length = Math.ceil(Math.random()*15);
     var randomPattern = ""
-    for (let i = 0; i<= length; i++) {
+    for (let i = 0; i <= length; i++) {
         const letter = Math.floor(Math.random()*8)
         randomPattern += array[letter]
     }
     return randomPattern
 }
+
+const generateRandomAxiom = () => {
+    const array = ["F", "+", "-", "*", "/"]
+    const length = Math.ceil(Math.random()*500)
+    var randomAxiom = ""
+    for (let i = 0; i <= length; i++) {
+        const letter = Math.floor(Math.random()*5)
+        randomAxiom += array[letter]
+    }
+    return randomAxiom
+}
+
+const draw = () => {
+    for (let i = 1; i <= points.length-1; i++) {
+        const planeGeometry = new THREE.PlaneGeometry(1, 1)
+        const plane = new THREE.Mesh(planeGeometry, material)
+        const dx = points[i].x - points[i-1].x
+        const dz = points[i].z - points[i-1].z
+        const distance = Math.sqrt(dx*dx+dz*dz)
+        const angle = Math.atan2(dz, dx)
+        plane.scale.set(distance, 1, 1)
+        plane.position.set((points[i].x + points[i-1].x)/2, 0, (points[i].z + points[i-1].z)/2)
+        plane.rotation.z = angle
+        plane.rotation.x = Math.PI/2
+        scene.add(plane)
+    }
+}
+
+// AXIOMES ---------------------------------------------------------
+const testAxiom = "FFF[+FF-FF]FFFF/FFF[+FF-FF]FFFF/FFF[+FF-FF]FFFF"
+// const testAxiom = "F[-F]F[+F]F"
+const axiom1 = "F"
+const axiom2 = "F-F+*F/F"
+const axiomGPT100 = "F*F*F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+F/F/F*F/F/F+"
+const axiomGPT200 = "F++--FF+F+FF-FF/FFFF/F/+F-F/F+F/FFF/F+/FF/F/FFF/F/F/F+F/F/F+FFF/F+F/F/FF/F/F+F/F/FFF/F+F/F/FFF/F/F+F/F/F+FFF/F+F/F/FF/F/F+F/F/FFF/F+F/F/FF/F/F+F/F/FFF/F+F/F/FF/F/F+F/F/FFF/F+F/F/FF/F/F+F/F/FFF/F+F/F/F"
+const axiomGPT300 = "F+-*F/FF*F+F*-*F/F*FF*+FF*FF-+F*FF/F-F*F*F-FF*F*FF+*F-FF/F+F/FF-F/F-FF*F-FF/F-FF-F-FF/F*F/FF/F*F+F/FF*F-F*FF+F*FF/FF*F/FF+FF/FF*F*F*FF/F*F/FF/FF+FF/F/F-F/F-F-FF+F+F/F*FF/FF/F*F/FF*F/FF/F*F/FF*F*F/FF+FF/F/F*FF+F/FF*F/FF*F/FF*F/FF/F*F+F/FF*F/F*F+F+F/FF*F/FF*F/FF*F/FF*F/FF*F+F*F/FF/F*F+F+F*F/FF/F*F*FF/"
+const axiomGPT400 = "F+FF-FF*F/FF*+FF/F-F*F*FF/F*F/F+FF-FF/FF+F*F/F/F-F/F*FF/F-F*F*F/FF*FF-F/F-F/F*F+FF/F+FF*FF/FF/F*F/F*FF-F*F/F/F*F/FF/F+FF-F/F+F/F/F*FF/FF-F+F/F/FF*F/FF/F*FF*F*F/F*F/F*FF/FF*F*F*F/FF-F/F*F/FF+F/FF/F/F*F/FF/F/F*FF-F/F/F*F/FF/F*F+F/FF/FF+F/F/F*F/FF/F*FF/FF/F+F/F+F/F*F/F/FF/F/F*FF/FF+F*F/F*F/FF*F+F/F*F/F/FF+F/FF/F+F/F/F*F/FF/F/F*F/F+F/FF/F*FF-F/F/F+F/F/F*F/F+F+F/FF+F/FF*F+F+F/FF/F+F/FF/FF*F+F+F/FF/F*F/"
+const axiomGPT500 = "FF+--*FF+*F-FF*F/FF+*FF+FF/FF+FF*F/FF*FF+F-FF*F/F/F/F/F/F/F/FF*F/FF*FF+F/F/F/FF+*F/F/F*FF/F/F/F/F/FF+F*FF/FF*F/F/F/F/F/F/FF/F/FF/F/F/FF+F/FF/F/F/FF*F/F/F/F/FF+*FF/FF/FF/F/F/F/F/FF*F/F/FF/FF/F/FF+*FF+F+F/FF/F/F/F/F/F/F/F/FF/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F/F"
+const axiomGPT852 = "F*+*-*F/+F+F-FFF+FF/F*-*+F+F*+F+*F*-*+F-F*-*F+F*-*+F*+FF/+F/F-+*F++F/F-+FF-F/FF-F/FF/F+F/FF/F-F/FF/+F-F+*F*+F-F/FF/F-+FF*-*+F/+F/+*F-*F/F+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+*F-F*-*F-F+F/F*-*F/F+F/+F/FF+F/FF/F*-*F/+F/+F/F+F*+FF/+F+F/+F/F+F*+FF/F-+*F/F-F/+F+F/F-F/FF*+FF/F-+FF/F+F-+"
+const axiomBard100 = "+F++F++F*F+F-**/F-F-F*F++F-**F+*F*-F+**F--F-F+*++F++F-F-F-FF++F+F*-F-*FF-F**F+F-F+*F-**F-FF-F**F--FF"
+const axiomBard200 = "---F****+/F+F/*///+FF/F++**//++*///+*****FFF-+/--*F-*-F--*--++FF++*F+FF-+/+*+/+F+-++*/FFF/++F/-/-+/*F+/F*/F-/F+*+*+-*+F-F//-**F/F+F++F--*/***+--F+F//+//*/--+FF-//+F--+**F-/++-F*F/--/F+FF--FF-+*-F--/FF"
+const axiomBard300 = "F+/F+-+*/++F-/*+-+F*FF*F++**--F+/F+F+FF/F+F/+**-+-+F+FF++F+**F-F+*-F+F+-*/F+F+-*/F-F/+F+/F+F++*F++F+-FF+F+F*-*/F++/F+F+F+F/-F+F+F*-F+F+-*/F+F+-*/F-F/+F+/F+F++*F++F+-FF+F+F*-*/F++/F+F+F+F/-F+F+F*-F+F+-*/F+F+-*/F-F/+F+/F+F++*F++F+-FF+F+F*-*/F++/F+F+F+F/-F+F+F*-F+F+-*/F+F+-*/F-F/+F+/F+F++*F++F+-FF+F+F*"
+const axiomBard400 = "/+F+/++*-F-/F---/--//+//-F-F-/---//+F++F**/F-/F/F**F+/+++-FF-F++-/-/F+/F+FFF+///F/+-+FF/+*-/FF-F-/FF+///-+F/**+/F-++/F++/-FF-+-/-F/--F/+//+F-+F+/+-///++/F+F**/+F--/--+**//++FFFFF-+-///-F/-F+--+F--/F---/+//FFFFF+//+/F-+F++F+F-+F+F-+/FF+F++/-++/+/F//F++++-F-//FF/F-FFF+/F/FF-F/F-+-+F//+FF/+*-++-+/+F-F-/++F/---+--++-+-F+F-F-F/-/F+/+--+-+-*F+-/-++//FF///-F+++//-+/+-F-----+FF/-F/+F-FF+FF+-++/F+/-FF-/F*/"
+const axiomBard500 = "*F+/-*/++//*-**++*F/F**/-++-+/F/+/++F/-+///FF-//+F/+/-++F+**--F/+**/-F/+/FF*-/-F-FF*--*-/F--//F-F*-+/+*-F-F**/F-*+F-F///---/-F*-+*-//+-F--F--/F/-/*-+*+**/*-F-/++F/*F*FF+-FF++**/*/*-*/++/+/--/F+-+***-*+-/F****-*/F**F*-*F-F-*/+*F*+-*F+-F+F+F*F-+*+**/-**-F-*-///-F/-*/*/FFFF-+FFF//F+-F--FF**+++-*++*/+*//F*+-+/*-+/+-+F+//F//FFF+-FF/+---F/+F*/F+-+/FF+---F**F-///*//+-+++**F*F/F-***//-*-F+**+*FF*+F*FF+-F/---FF/+*F+-F-F-+/+*FF*//+-*F-/FFF-+--/F/FF--F/F*F/-*+**+*++/F/*/+F/*/-*F+*/-F+-FF*/-/*+----F---***F-"
+const randomAxiom = generateRandomAxiom();
+// -----------------------------------------------------------------
 
 // PATTERNS --------------------------------------------------------
 const patternF1 = ""
@@ -52,7 +94,7 @@ Number.prototype.mod = function (n) {
 // définition de la scene et de la caméra
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1100000 );
-camera.position.z = 5
+camera.position.y = 20
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -102,13 +144,14 @@ let angles = [
     new THREE.Vector3(5, 0, -5)
 ]
 
-const axiomArray = [...axiom]
-const selectedPatternF = [...randomPatternF]
-const selectedPatternPlus = [...randomPatternPlus]
-const selectedPatternMinus = [...randomPatternMinus]
-const selectedPatternTimes = [...randomPatternTimes]
-const selectedPatternDiv = [...randomPatternDiv]
+const axiomArray = [...testAxiom]
+const selectedPatternF = [...patternF1]
+const selectedPatternPlus = [...patternPlus1]
+const selectedPatternMinus = [...patternMinus1]
+const selectedPatternTimes = [...patternTimes1]
+const selectedPatternDiv = [...patternDiv1]
 
+console.log("length", axiomArray.length)
 console.log("selectedPatternF", selectedPatternF)
 console.log("selectedPatternPlus", selectedPatternPlus)
 console.log("selectedPatternMinus", selectedPatternMinus)
@@ -147,8 +190,15 @@ while (axiomArray[i] != undefined) {
     }
 }
 
+console.log(axiomArray)
+
+
+
 var points = [];
 points.push(new THREE.Vector3(x, 0, z))
+var savedPos = new THREE.Vector3(0, 0, 0)
+var savedDir = dir
+var p = 0;
 
 axiomArray.map((letter) => {
     var currentAngle = angles[dir]
@@ -156,6 +206,7 @@ axiomArray.map((letter) => {
     if (letter == "F") {
         x += currentAngle.x;
         z += currentAngle.z;
+        p += 1
         points.push(new THREE.Vector3(x, 0, z));
     } else if (letter == "+") {
         dir = (dir+1).mod(8)
@@ -169,27 +220,32 @@ axiomArray.map((letter) => {
     } else if (letter == "/") {
         dir = (dir-2).mod(8)
         currentAngle = angles[dir];
+    } else if (letter == "[") {
+        console.log("[", "points", points, "points p", points[p])
+        savedPos = points[p]
+        savedDir = dir
+    } else if (letter == "]") {
+        console.log("]", "saved pos", savedPos, "points", points)
+        draw()
+        x = savedPos.x
+        z = savedPos.z
+        dir = savedDir
+        points = []
+        p = 0
+        points.push(new THREE.Vector3(x, 0, z))
     }
 })
+console.log(points)
+draw()
 
-for (let i = 1; i <= points.length-1; i++) {
-    const planeGeometry = new THREE.PlaneGeometry(1, 1)
-    const plane = new THREE.Mesh(planeGeometry, material)
-    const dx = points[i].x - points[i-1].x
-    const dz = points[i].z - points[i-1].z
-    const distance = Math.sqrt(dx*dx+dz*dz)
-    const angle = Math.atan2(dz, dx)
-    plane.scale.set(distance, 1, 1)
-    plane.position.set((points[i].x + points[i-1].x)/2, 0, (points[i].z + points[i-1].z)/2)
-    plane.rotation.z = angle
-    plane.rotation.x = Math.PI/2
-    scene.add(plane)
-}
+
+
+
 
 // boucle de rendu
 function animate() {
     requestAnimationFrame( animate );
-controls.update()
+    controls.update()
     renderer.render( scene, camera );
 }
 
