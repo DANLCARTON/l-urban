333e0afc9a29663e6b74f196602913fa695357be
Eric Thiberge <78015609+DANLCARTON@users.noreply.github.com>
2023-10-02 12:08:13 +0200

    Update rules.js

--- rules.js
+++ rules.js
@@ -15,6 +15,14 @@ const FRules = {
     "patternF4" : "F-F*F-F",
     "patternF5" : "F/F**FF**F/F/F",
     "patternF6" : "F/FF*F**F/F*F*F**F/FF*F",
+    "patternF7" : "-F+F+F+F+F+F+F+F+F+F+F-F",
+    "patternF8" : "F-F+F+F-F",
+    "patternF9" : "F/F*F*F/F*F*F/F*F*F/F*F*F/F*F*F/F*F/F",
+    "patternF10" : "FFF/F+*FF*FF+*F/FFF*FF",
+    "patternF11" : "*FF-/FF+*FF",
+    "patternF12" : "*FFF/FF/FF",
+    "patternF13" : "-F*FF/F/F/FF*F*F*FF/F+F",
+    "patternF14" : "F-F+F*+F-F-F+*F+F-F*F",
     "randomPatternF" : generateRandomPattern(["F", "F", "F", "F", "+", "-", "*", "/"]),
 }
 
